3cde2d1e83b4a309248c0e61eaf420913102e6e0
Angtens <tanhpde180161@fpt.edu.n>
2025-06-22 17:00:31 +0700

    Update appsetting

--- HuynhPhucTanWPF/App.xaml.cs
+++ HuynhPhucTanWPF/App.xaml.cs
@@ -1,14 +1,26 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 
 namespace HuynhPhucTanWPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static IConfiguration Configuration { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var email = Configuration["DefaultAdminAccount:Email"];
+            var password = Configuration["DefaultAdminAccount:Password"];
+
+        }
+    }
 }
--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7969261a3c7c7a29b6781a57677c73fac3dc8682")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
